72c21bf44afe3b7e2fe64e6bc0909b2806ea5969
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2022-07-25 16:13:39 +0700

    chore: add rollup to build

---
+++ rollup.config.js
@@ -0,0 +1,50 @@
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import del from 'rollup-plugin-delete';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import ttypescript from 'ttypescript';
+import pkg from './package.json';
+
+export default {
+	input: 'src/index.tsx',
+	output: [
+		{
+			format: 'cjs',
+			dir: 'dist/cjs',
+			preserveModules: true,
+			preserveModulesRoot: 'src',
+			exports: 'named',
+		},
+		{
+			format: 'es',
+			dir: 'dist/esm',
+			preserveModules: true,
+			preserveModulesRoot: 'src',
+			exports: 'named',
+		},
+	],
+	external: [
+		...Object.keys(pkg.dependencies || {}),
+		...Object.keys(pkg.peerDependencies || {}),
+		'./src',
+	],
+	plugins: [
+		peerDepsExternal(),
+		typescript({
+			typescript: ttypescript,
+			tsconfig: './tsconfig.build.json',
+		}),
+		nodeResolve(),
+		commonjs(),
+		babel({
+			babelHelpers: 'runtime',
+			exclude: 'node_modules/**',
+			extensions: ['.ts', '.tsx'],
+		}),
+		terser(),
+		del({ targets: 'dist/*' }),
+	],
+};
