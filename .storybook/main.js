eed8265bf75ecf5d61fd15d60411cb09ec7a3078
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2022-08-02 23:07:02 +0700

    chore: add npmrc file

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,25 +5,13 @@ module.exports = {
 		builder: 'webpack5',
 	},
 	webpackFinal: async (config) => {
-		config.module.rules.push({
-			test: /\.(ttf|woff|woff2|otf|svg)$/,
-			use: {
-				loader: 'file-loader',
-				options: {
-					name: '[name].[ext]',
-				},
-			},
-			include: path.resolve(__dirname, '../'),
-		});
 		config.stats = 'errors-only';
 		config.resolve.modules.push('src');
-
 		return config;
 	},
 	framework: '@storybook/react',
 	stories: [
 		'../src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-		'../src/**/__stories__/*.stories.@(js|jsx|ts|tsx|mdx)',
 		'./**/__stories__/*.stories.@(js|jsx|ts|tsx|mdx)',
 	],
 	addons: [
