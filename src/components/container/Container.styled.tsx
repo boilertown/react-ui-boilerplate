e61c077b6f749323c2c08539d7ee8f6e673b02f8
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2022-09-27 13:45:01 +0700

    chore: update packages version

--- src/components/container/Container.styled.tsx
+++ src/components/container/Container.styled.tsx
@@ -3,12 +3,16 @@ import { SCREEN_LG, SCREEN_XM } from 'constants/breakpoints';
 import { COLUMN_WIDTH, GUTTER } from 'constants/spacing';
 
 export const FluidContainer = styled.div`
+	margin-left: auto;
+	margin-right: auto;
 	width: 100%;
 `;
 
 export const Container = styled.div`
 	width: 100%;
 	max-width: ${COLUMN_WIDTH * 4 + GUTTER * 2 * 3}px;
+	margin-left: auto;
+	margin-right: auto;
 
 	@media (min-width: ${SCREEN_XM}px) {
 		max-width: ${COLUMN_WIDTH * 8 + GUTTER * 2 * 7}px;
